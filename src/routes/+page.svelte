d2f1908a31d30cf484b3a24ef60e0d87c627d1f5
refact0r <yifanw06@gmail.com>
2024-03-03 10:40:35 -0800

    home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,5 @@
 <script>
-	import IconSearch from '~icons/ph/magnifying-glass';
-	import logo from '$lib/assets/papercut-logo.png';
+	import IconSearch from '~icons/ph/magnifying-glass-bold';
 	import { goto } from '$app/navigation';
 	let params = '';
 
@@ -18,55 +17,75 @@
 	<div class="logo">
 		<h1><span class="paper">paper</span><span class="slash">/</span>cut</h1>
 	</div>
-	<div class="search">
-		<form on:submit|preventDefault={() => searchQuery()}>
-			<input
-				type="text"
-				placeholder="search..."
-				bind:value={params}
-				oninvalid="this.setCustomValidity('please enter a valid search term! only letters and numbers are allowed.')"
-				oninput="this.setCustomValidity('')"
-				pattern="[a-zA-Z0-9]+"
-				required
-			/>
-			<button type="submit" class="submit"><IconSearch /></button>
-		</form>
-		<br />
-		<a href="/adv-search" class="adv">advanced search</a>
-	</div>
-	<br />
-	<div class="links">
-		<div class="column">
-			<a href="/about" class="options">about/help</a>
-		</div>
-		<div class="column">
-			<a href="/" class="options">settings</a>
-		</div>
-		<div class="column">
-			<a href="/" class="options">github ↗</a>
-		</div>
+	<form on:submit|preventDefault={() => searchQuery()}>
+		<button type="submit" class="submit"><IconSearch style="font-size: 1.4rem;" /></button>
+		<input type="text" placeholder="search..." bind:value={params} required />
+	</form>
+	<div class="row">
+		<h2>find papers fast.</h2>
+		<a href="/settings" class="options">settings</a>
+		<a href="/about">about</a>
 	</div>
 </div>
 
 <style lang="scss">
 	.content {
-		margin: 5rem;
+		padding: 5rem;
 		height: 100%;
 		width: 100%;
 	}
 
 	h1 {
-		font-size: 5rem;
+		font-size: 7rem;
 		letter-spacing: -0.015em;
-
+		margin: 10% 0 0 0;
 		.paper {
 			display: inline-block;
 			vertical-align: top;
+			margin-top: 0.05em;
 		}
 
 		.slash {
-			font-size: 7rem;
+			font-size: 9.5rem;
 			font-weight: 500;
+			letter-spacing: -0.05em;
+			margin-left: -0.04em;
+		}
+	}
+
+	h2 {
+		color: var(--fg-2);
+	}
+
+	.row {
+		@include flex(row, left, center);
+
+		gap: 2rem;
+	}
+
+	form {
+		background-color: var(--bg-2);
+		border-bottom: 4px solid var(--fg);
+		@include flex(row, left, center);
+		width: 100%;
+
+		button {
+			padding: 0.8rem;
+			background: none;
+			@include flex(column, center, center);
+		}
+
+		input {
+			padding: 0.8rem 0.8rem 0.8rem 0;
+			font-size: 1.4rem;
+			width: 100%;
+		}
+	}
+
+	a {
+		font-size: 1.3rem;
+		&.options {
+			margin-left: auto;
 		}
 	}
 </style>
