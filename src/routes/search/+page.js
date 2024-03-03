658a710f342cc0f2c72c4a41dc85eceaca9f6fba
refact0r <yifanw06@gmail.com>
2024-03-02 17:45:09 -0800

    basic load

--- src/routes/search/+page.js
+++ src/routes/search/+page.js
@@ -1,4 +1,9 @@
-export function load({ params }) {
-	console.log(params);
+import query from '$lib/arxiv.js';
+
+export async function load({ url, fetch }) {
+	let results = await query({ query: url.searchParams.get('q') }, fetch);
+
+	console.log(results);
+
 	return {};
 }
