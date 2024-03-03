04e6d25b8dc9880fe69615d60baffddaf5faacbd
shibest <eerkingtheawesome@gmail.com>
2024-03-03 00:30:19 -0800

    ui updates and some functionality stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,18 @@
 <script>
 	import IconSearch from '~icons/ph/magnifying-glass';
-
+	import logo from '$lib/assets/papercut-logo.png';
+	import { goto } from '$app/navigation';
+	import { onMount } from 'svelte';
 	let params = '';
+	let search;
+	onMount(() => {
+		search.addEventListener('keypress', function (event) {
+			if (event.key === 'Enter') {
+				event.preventDefault();
+				goto(`/search?q=${params}`);
+			}
+		});
+	});
 </script>
 
 <svelte:head>
@@ -11,9 +22,15 @@
 
 <div class="content">
 	<div class="container">
-		<h1>paper<span class="slash">/</span>cut</h1>
-		<input type="text" placeholder="search..." bind:value={params} />
-		<a class="submit" href={`/search?q=${params}`}><IconSearch /></a>
+		<!--<h1>paper<span class="slash">/</span>cut</h1>-->
+		<img src={logo} alt="logo" style="width: 18rem;" />
+		<br />
+		<div class="search">
+			<input type="text" placeholder="search..." bind:value={params} bind:this={search} />
+			<a class="submit" href={`/search?q=${params}`}><IconSearch /></a>
+			<br />
+			<a href="/advsearch" style="float: right; padding: .25rem 1rem 0rem 0rem;">Advanced Search</a>
+		</div>
 	</div>
 </div>
 
@@ -25,10 +42,40 @@
 		width: 100%;
 	}
 
-	.slash {
+	.search {
+		padding: 2rem 0rem;
+	}
+
+	input[type='text'] {
+		outline: none;
+		border: none;
+		border-radius: 4rem;
+		height: 4rem;
+		width: 32rem;
+		padding: 0rem 1rem;
+		font-size: 1.5rem;
+	}
+
+	.submit {
+		position: absolute;
+		font-size: 1.5rem;
+		margin-top: 0.7rem;
+		margin-left: -3rem;
+		text-decoration: none;
+		color: black;
+		border-radius: 2rem;
+		transition: 0.1s;
+		padding: 0.2rem;
+	}
+
+	.submit:hover {
+		background-color: rgba(0, 0, 0, 0.25);
+	}
+
+	/*.slash {
 		position: absolute;
 		font-weight: 700;
 		margin-left: 0.15ch;
 		margin-top: 0.1ch;
-	}
+	}*/
 </style>
---
+++ src/routes/advsearch/+page.svelte
@@ -0,0 +1,4 @@
+<svelte:head>
+	<title>advanced search - papercut</title>
+	<meta name="description" content="papercut advanced search page" />
+</svelte:head>
