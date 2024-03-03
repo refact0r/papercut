a36760390dc979e8a9659516ecaf57adc252d080
NicoNekoru <66227158+NicoNekoru@users.noreply.github.com>
2024-03-02 18:41:01 -0800

    Usable for client but information was lost in xml parsing?

--- src/lib/arxiv.js
+++ src/lib/arxiv.js
@@ -1,59 +1,42 @@
-import { parseString } from "xml2js";
-import { promisify } from "util";
-
-const parseXmlPromise = promisify(parseString);
+import { XMLParser } from 'fast-xml-parser'
 
 /**
- * The object schema that arxiv returns is:
-	{
-		id: [ string ],
-		updated: [ string ],
-		published: [ string ],
-		title: string[],
-		summary: string[],
-		author: { name: [Array] }[],
-		'arxiv:doi': { _: 'string', '$': [Object] }[],
-		link: { '$': [Object] }[],
-		'arxiv:primary_category': [ { '$': [Object] } ],
-		category: { '$': [Object] }[]
-	}
- which has a lot of redundency. This function parses it into a more usable format
  * @param {Object} param0 
- * @param {string[]} param0.id
- * @param {string[]} param0.updated
- * @param {string[]} param0.published
- * @param {string[]} param0.title
- * @param {string[]} param0.summary
- * @param {string[]} param0.author
+ * @param {string} param0.id
+ * @param {string} param0.updated
+ * @param {string} param0.published
+ * @param {string} param0.title
+ * @param {string} param0.summary
+ * @param {string} param0.author
  * @param {string[]} param0.links
  * @param {string[]} param0.category
  * @returns {Object}
  */
 function parseArxivObject({
-	id: [id],
-	updated: [updated],
-	published: [published],
+	id,
+	updated,
+	published,
 	title,
 	summary,
 	author,
 	link,
 	category,
-	"arxiv:primary_category": [{$: primary_category}],
-	"arxiv:comment": [{_: comment}] = [{_: ''}],
-	"arxiv:affiliation": [{_: affiliation}] = [{_: ''}],
-	"arxiv:journal_ref": [{_: journal_ref}] = [{_: ''}],
-	"arxiv:doi": [{_: doi}] = [{_: ''}],
+	"arxiv:primary_category":  primary_category,
+	"arxiv:comment": comment = '',
+	"arxiv:affiliation": affiliation = '',
+	"arxiv:journal_ref": journal_ref = '',
+	"arxiv:doi": doi = '',
 })
 {
 	return {
 		id,
 		updated,
 		published,
-		title: title.join(''),
-		summary: summary.join(''),
-		author: author.map(a => a.name),
-		link: link.map(l => l.$),
-		category: category.map(c => c.$.term),
+		title,
+		summary,
+		author: author.name ? [author.name] : author.map(a => a.name),
+		link,
+		category,
 		primary_category,
 		comment,
 		affiliation,
@@ -70,7 +53,7 @@ function parseArxivObject({
  * @param {number} [param0.max_results = 10] Max results of the query 
  * @returns {Promise<{id: string, updated: string, published: string, title: string, summary: string, author: string, link: string, category: string, primary_category: string, comment: string, affiliation: string, journal_ref: string, doi: string}>} Returns the arxiv api's xml response parsed as an object
  */
-async function rawQueryArxiv({ query = "", ids = [], start = 0, max_results = 10 }, fetch = fetch)
+async function rawQueryArxiv({ query = "", ids = [], start = 0, max_results = 10 }, /* fetch = fetch */)
 {
 	if (!query && !ids.join("")) throw new Error('No id or query for the API');
 
@@ -82,7 +65,7 @@ async function rawQueryArxiv({ query = "", ids = [], start = 0, max_results = 10
 		`start=${start}&` +
 		`max_results=${max_results}`
 	);
-	const objectResponse = await parseXmlPromise(await apiResponse.text());
+const objectResponse = (new XMLParser()).parse(await apiResponse.text());
 	const usefulResponse = objectResponse.feed.entry;
 	return usefulResponse.map(parseArxivObject)
 }
