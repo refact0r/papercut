7b17c949d7e0ec35440ff5b0277e5d31d5973fd0
refact0r <yifanw06@gmail.com>
2024-03-02 17:49:47 -0800

    client run

--- src/routes/search/+page.js
+++ src/routes/search/+page.js
@@ -1,5 +1,7 @@
 import query from '$lib/arxiv.js';
 
+export const ssr = false;
+
 export async function load({ url, fetch }) {
 	let results = await query({ query: url.searchParams.get('q') }, fetch);
 
